911e8e23d3c56f3c496203f5df7532f3182a7b7d
Hazrul Amri <hazrulamri27@gmail.com>
2024-05-26 19:34:48 +0800

    Updated main.py
    - added input box
    - added box placeholder

--- main.py
+++ main.py
@@ -12,4 +12,10 @@ class MyToDoApp(tk.Tk):
         style = Style(theme='flatly')
         style.configure("Custon.TEntry", foreground="gray")
 
-        
+        #input box
+        self.tas_input = ttk.Entry(self, font=(
+            "TkDefaultFont",16), width=30,style="Custon.TEntry")
+        self.task_input.pack(pady=10)
+
+        #input box placeholder
+        self.task_input.insert(0,"What's your task today?")
