869d43874c880c179430bc697ac29690a2536c8c
Hazrul Amri <hazrulamri27@gmail.com>
2024-05-26 19:31:35 +0800

    updated main.py
    - class the main part (aspect ratio, title)
    - define initate

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from tkinter import ttk, messagebox
 from typing import Any
 from ttkbootstrap import Style
 import json
+class MyToDoApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("MyToDo App")
+        self.geometry("400x600")
+        style = Style(theme='flatly')
+        style.configure("Custon.TEntry", foreground="gray")
+
+        
