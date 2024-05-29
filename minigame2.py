44cfccc7147f4fcc701d7088fd739f53f3368c4a
AimanIswan05 <aimaniswan.n05@gmail.com>
2024-05-29 13:31:01 +0800

    updated
    adding layout properties

---
+++ minigame2.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+import random
+
+# layout properties
+WIDTH, HEIGHT = 800, 600
+BALL_RADIUS = 20
+PLATFORM_WIDTH, PLATFORM_HEIGHT = 100, 10
+FPS = 60
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 165, 0)
+LIGHT_BLUE = (173, 216, 230)
