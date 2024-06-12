b94fe566d7cac29e2be881185c90b55e5d34eb54
AimanIswan05 <aimaniswan.n05@gmail.com>
2024-06-12 13:39:13 +0800

    game screen
    - setting font size
    - created ball variable
    - setting ball speed
    - set lives limit

--- minigame2.py
+++ minigame2.py
@@ -13,3 +13,23 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 ORANGE = (255, 165, 0)
 LIGHT_BLUE = (173, 216, 230)
+
+pygame.init()
+
+# game screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("TeqBall Simulator")
+font = pygame.font.Font(None, 36)
+
+clock = pygame.time.clock()
+
+ball_pos = [WIDTH // 2, HEIGHT // 2]
+ball_speed = [random.uniform(2, 4), random.uniform(2, 4)]  
+platform_pos = [WIDTH // 2 - PLATFORM_WIDTH // 2, HEIGHT - PLATFORM_HEIGHT - 10]
+platform_speed = 10
+score = 0
+lives = 3
+current_level = 1
+platform_color = ORANGE
+
+
